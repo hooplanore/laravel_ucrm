1fb141c92d7e3ca325b366ff5a02569aa6acd9cd
hooplanore <hooplanore@gmail.com>
2023-01-09 09:19:36 +0900

    TestController

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+
+</script>
+<template>
+テストです。
+<a href="/">aタグです</a><br>
+<Link href="/">Link経由です</Link><br>
+<Link :href="route('inertia.index')">名前つきルートです</Link><br>
+<Link :href="route('inertia.show',{id: 100 })">ルートパラメータテストです</Link>
+</template>
